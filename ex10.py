4810da0acb3554b5d1635c7b56ea6cb37286bea5
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-03-24 13:54:59 +0700

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,6 +1,5 @@
 x1, y1, x2, y2 = map(int, input().split())
 x3, y3, x4, y4 = map(int, input().split())
-
 if x2 <= x3 or x4 <= x1 or y2 <= y3 or y4 <= y1:
     print("Прямоугольники лежат вне друг друга, не касаясь")
 elif (x2 == x3 or x1 == x4) and (y2 == y3 or y1 == y4):
